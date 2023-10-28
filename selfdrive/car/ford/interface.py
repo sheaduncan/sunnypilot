bf9f6957951c8dbf9205511876e47fb0c98acadc
Isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2023-10-28 00:37:03 -0400

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -158,7 +158,6 @@ class CarInterface(CarInterfaceBase):
 
     events, ret = self.create_sp_events(self.CS, ret, events)
     
-    events = self.create_common_events(ret, extra_gears=[GearShifter.manumatic])
     if not self.CS.vehicle_sensors_valid:
       events.add(car.CarEvent.EventName.vehicleSensorsInvalid)
     if self.CS.hybrid_platform:
